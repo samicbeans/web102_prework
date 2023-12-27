90725ee42ec1a216daa67b3400b7070f4e891256
samicbeans <sami.chen28@myhunter.cuny.edu>
2023-12-27 13:11:49 -0500

    challenge 3 complete

--- index.js
+++ index.js
@@ -27,26 +27,32 @@ const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
-
+    for(let i = 0; i < games.length; i++){
     // loop over each item in the data
-
-
         // create a new div element, which will become the game card
-
-
-        // add the class game-card to the list
-
+        const newdiv = document.createElement("div");
+        // add the class game-card to the div's class list
+        newdiv.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
+        newdiv.innerHTML = 
+            `<img class = "game-img" src=${games[i].img} /> 
+            <h4> ${games[i].name} </h4>
+            <p>Description: ${games[i].description} </p>
+            <p>Pledged: ${games[i].pledged}
+            
+            Goal: ${games[i].goal}</p>
+            `;
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
 
         // append the game to the games-container
-
+        gamesContainer.appendChild(newdiv);
+    }
 }
-
+addGamesToPage(GAMES_JSON);
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
 
